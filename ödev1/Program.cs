5530eb609d2468be45ec1b1cfa5786ae5f5b4895
CAN <CAN@DESKTOP-8THKA1H>
2022-01-18 02:27:02 +0300

    bitti

--- ödev1/Program.cs
+++ ödev1/Program.cs
@@ -6,7 +6,27 @@ namespace ödev1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hamza1");
+            string cümle = "Türkcell gelecegi yazanlar";
+            string[] kelimeler = cümle.Split(' ');
+            Console.WriteLine("harf giriniz");
+            string harf =Console.ReadLine();
+
+            foreach (var kelime in kelimeler)
+            {
+                if (kelime.Contains(harf))
+                {
+                    Console.WriteLine("var");
+                }
+                else
+                {
+                    Console.WriteLine("yok");
+                }
+            }
+            foreach (var kelime in kelimeler)
+            {
+                Console.WriteLine(kelime);
+            }
+            
         }
     }
 }
