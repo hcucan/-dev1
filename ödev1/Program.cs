5d5cc36f1a05c7edb056a09492562ab43d5f1182
CAN <CAN@DESKTOP-8THKA1H>
2022-02-01 17:56:44 +0300

    ilk ödev

--- ödev1/Program.cs
+++ ödev1/Program.cs
@@ -10,8 +10,38 @@ namespace ödev1
             string[] kelimeler = cümle.Split(' ');
             Console.WriteLine("harf giriniz");
             string harf =Console.ReadLine();
+            kelimeVarmi(cümle,harf,kelimeler);
+            basHarfBulama(cümle, harf);
+            harfVarmi(cümle, harf);
+            bastanBoslukBirakma(cümle);
+            cümledenSonraBoslukbirakma(cümle);
+        }
 
-            foreach (var kelime in kelimeler)
+        private static void cümledenSonraBoslukbirakma(string cümle)
+        {
+            Console.Write(cümle.PadRight(35));
+            Console.WriteLine("hamza");
+        }
+
+        private static void bastanBoslukBirakma(string cümle)
+        {
+            string cümle2 = cümle.PadLeft(35);
+            Console.WriteLine(cümle2);
+        }
+
+        private static void harfVarmi(string cümle, string harf)
+        {
+            Console.WriteLine(cümle.EndsWith(harf));
+        }
+
+        private static void basHarfBulama(string cümle,string harf)
+        {
+            Console.WriteLine(cümle.StartsWith(harf));
+        }
+
+        public static void kelimeVarmi(string cümle,string harf,string[] kelimeler)
+        {
+             foreach (var kelime in kelimeler)
             {
                 if (kelime.Contains(harf))
                 {
@@ -26,7 +56,6 @@ namespace ödev1
             {
                 Console.WriteLine(kelime);
             }
-            
         }
     }
 }
